490d50841a968c140f51c062bdd81d9de2a4a5d7
Maciej Kutrowski <mkutrows@mion.elka.pw.edu.pl>
2018-05-25 22:59:11 +0200

    Merge branch 'master' of E:\Users\Nexon\git\CheckersNegaScout4 with conflicts.

--- src/checkers/StartUp.scala
+++ src/checkers/StartUp.scala
@@ -1,33 +1,27 @@
 package checkers
 
-import checkers.findingNextMove.MoveSet
-
 
 object StartUp {
   def main(args: Array[String]): Unit = {
-    var board = new Board();
-    var negaScout = new NegaScout();
-    board.setUpBoard();
-    board.printBoard();
-  //TODO sprawdzanie czy koniec gry przed kazdym ruchem
+    var board = new Board()
+    var negaScout = new NegaScout()
+    board.setUpBoard()
+    board.printBoard()
+
     while (!board.isFinished()) {
       print("Enter move: "); // input form ex. 60 51
-      val x = board.getAllMoveSetsForColor(Type.white)
-      println(board.getAllPossibleMoves(Type.white));
-      var input = scala.io.StdIn.readLine();
-      if (!board.checkIfMoveCorrect(input, Type.white)){
-        println("wrong move!");
+      var input = scala.io.StdIn.readLine()
+      if (!board.checkIfMoveCorrect(input)){
+        println("wrong move!")
       }
       else {
-        board.makeMoveSequence(input);
-        board.printBoard();
-        println("enemy making move")
-        var enemysMove = negaScout.getBestMove(board);
-        board.makeMoveSequence(enemysMove);
-        board.printBoard();
+        board.makeMoveSequence(input)
+        var enemysMove = negaScout.getBestMove(board)
+        //board.makeMoveSequence(enemysMove);
+        board.printBoard()
       }
     }
 
-    print("End of the game!");
+    print("End of the game!")
   }
 }
